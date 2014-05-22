8dc207a31c969b9b8181b9bd409970505f0136a7
Greg Fitzgerald <greg@gregf.org>
2014-05-22 13:48:32 -0400

    Update for ruby 2.1

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
-[fpm-ruby](https://github.com/stankevich/fpm-ruby)
-======
+#[fpm-ruby](https://github.com/gregf/fpm-ruby)
 
-[fpm-cookery](https://github.com/bernd/fpm-cookery) recipe for building Ruby 1.9.3 .deb package.
+[fpm-cookery](https://github.com/bernd/fpm-cookery) recipe for building Ruby 2.1.2 .deb package.
 
 ## Usage
 
@@ -11,5 +10,6 @@
 
 ## Authors
 
+* [Greg Fitzgerald](https://github.com/gregf)
 * [Sergey Stankevich](https://github.com/stankevich)
 * [bernd](https://github.com/bernd)
--- recipe.rb
+++ recipe.rb
@@ -1,17 +1,17 @@
 class Ruby193 < FPM::Cookery::Recipe
   description 'An interpreter of object-oriented scripting language Ruby'
-  name        'ruby1.9.3'
-  version     '1:1.9.3.194'
+  name        'ruby2.1'
+  version     '1:2.1.2'
   revision    '1'
   homepage    'http://www.ruby-lang.org/'
-  source      'http://ftp.ruby-lang.org/pub/ruby/1.9/ruby-1.9.3-p194.tar.bz2'
-  sha256      'a9d1ea9eaea075c60048369a63b35b3b5a06a30aa214a3d990e0bb71212db8fa'
+  source      'http://cache.ruby-lang.org/pub/ruby/2.1/ruby-2.1.2.tar.gz'
+  sha256      'f22a6447811a81f3c808d1c2a5ce3b5f5f0955c68c9a749182feb425589e6635'
   arch        'x86_64'
   section     'interpreters'
 
-  build_depends 'build-essential', 'autoconf', 'libreadline5-dev', 'bison',
+  build_depends 'build-essential', 'autoconf', 'libreadline6-dev', 'bison',
                 'zlib1g-dev', 'libssl-dev', 'libyaml-dev'
-  depends       'libreadline5', 'zlib1g', 'openssl', 'libyaml-0-2'
+  depends       'libreadline6', 'zlib1g', 'openssl', 'libyaml-0-2'
 
   def build
     configure :prefix => '/usr/local', 'disable-install-doc' => true
